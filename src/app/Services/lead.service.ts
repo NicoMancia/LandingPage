b735ee98aeaeb038abd555e8679c92555f8dfe19
nicola <nicolamancia01@gmail.com>
2024-05-21 17:29:01 +0200

    environment

--- src/app/Services/lead.service.ts
+++ src/app/Services/lead.service.ts
@@ -1,31 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LeadService {
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
   //Observable --> perché dobbiamo far comunicare il frontend con il backend
-  create(data: any): Observable<any>{
-    return this.httpClient.post('http://localhost:3000/lead', data);
+  create(data: any): Observable<any> {
+    return this.httpClient.post(`${environment.endpointBE}lead`, data);
   }
 
-  findAll(): Observable<any>{
-    return this.httpClient.get('http://localhost:3000/lead');
+  findAll(): Observable<any> {
+    return this.httpClient.get(`${environment.endpointBE}lead`);
   }
 
-  findOne(id: number): Observable<any>{
-    return this.httpClient.get(`http://localhost:3000/lead/${id}`);
+  findOne(id: number): Observable<any> {
+    return this.httpClient.get(`${environment.endpointBE}lead/${id}`);
   }
 
-  update(id: number,data: any): Observable<any>{
-    return this.httpClient.patch(`http://localhost:3000/lead/${id}`, data);
+  update(id: number, data: any): Observable<any> {
+    return this.httpClient.patch(`${environment.endpointBE}lead/${id}`, data);
   }
 
-  remove(id: number): Observable<any>{
-    return this.httpClient.delete(`http://localhost:3000/lead/${id}`);
+  remove(id: number): Observable<any> {
+    return this.httpClient.delete(`${environment.endpointBE}lead/${id}`);
   }
 }
